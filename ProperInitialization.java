80bf9dc86c584a83f2a7dcde3aad62c84316fe3b
Manish Khadka <mnzkhadka1996@gmail.com>
2024-12-09 02:15:59 +0545

    proper initialization in java

---
+++ ProperInitialization.java
@@ -0,0 +1,7 @@
+public class ProperInitialization {
+    public static void main(String[] args) {
+        int[] numbers = new int[3]; // Initialized with default values (0, 0, 0)
+        numbers[0] = 10;           // Assigning values
+        System.out.println(numbers[0]); // Outputs: 10
+    }
+}
